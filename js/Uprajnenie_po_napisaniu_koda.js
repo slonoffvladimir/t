783eedc8000004991be65433a54ecbda2981d29d
wowa <slonovvladimir.com@gmail.com>
2023-08-07 16:46:52 +0300

    first commit

---
+++ js/Uprajnenie_po_napisaniu_koda.js
@@ -0,0 +1,6 @@
+"unis strict";
+let userName ="John";
+let number=25;
+number=24;
+console.log(userName);
+console.log(number);
---
+++ js/uprajnenie_2.js
@@ -0,0 +1,13 @@
+
+// eslint-disable-next-line no-unused-vars
+let storeName="Gudvin";
+// eslint-disable-next-line no-unused-vars
+const storeDescription={
+    budget: 100000,
+    employees:["Inna","Irina","Katerina"],
+    products:{
+        moloko:25,
+        pivo:70
+    }
+
+};
